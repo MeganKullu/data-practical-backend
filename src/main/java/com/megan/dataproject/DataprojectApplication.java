0a14d496e3c900271baf5125ce1d0772235dc28d
Mkullu <kullu.megan@eclectics.io>
2026-02-15 22:47:05 +0300

    initialize project folders

---
+++ src/main/java/com/megan/dataproject/DataprojectApplication.java
@@ -0,0 +1,13 @@
+package com.megan.dataproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataprojectApplication.class, args);
+	}
+
+}
